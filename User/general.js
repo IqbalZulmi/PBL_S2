a6b57ff3be95746615d5a4e1ca98ff93834ea642
A.Iqbal Zulmi <zulmiiqbal@gmail.com>
2023-03-13 17:42:59 +0700

    update JS, Home,Sejarah,and Visi

--- User/general.js
+++ User/general.js
@@ -42,8 +42,31 @@ $('#offcanvasNavbar').on('hidden.bs.offcanvas', function () {
 });
 
 
-//lightbox review magnific
+//lightbox review and home magnific
 $(document).ready(function () {
+    $('.image-link').magnificPopup({
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			titleSrc: function(item) {
+				return item.el.attr('title');
+			}
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+		
+	});
     $('.btn-view').on('click', function (e) {
         e.preventDefault();
         var file = $(this).closest('.row').find('input[type=file]').prop('files')[0];
